52ab53fa69b08ca2906d193c04b6f17986a1ff59
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-12-18 13:39:03 +0200

    Initial commit of the project skeleton for Common Lisp

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM clfoundation/sbcl:2.2.4
+
+ENV QUICKLISP_ADD_TO_INIT_FILE=true
+ENV QUICKLISP_DIST_VERSION=latest
+
+WORKDIR /app
+COPY . .
+
+RUN mkdir -p ~/.config/common-lisp/source-registry.conf.d && \
+    echo '(:tree "/app/")' >  ~/.config/common-lisp/source-registry.conf.d/workspace.conf && \
+    /usr/local/bin/install-quicklisp
+
+ENTRYPOINT ["./scripts/run-tests.sh"]
---
+++ build-skeleton.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+#
+# Utility script for creating new projects using a skeleton
+#
+
+set -e
+
+_SCRIPT_NAME="${0##*/}"
+_SCRIPT_DIR=$( dirname `readlink -f -- "${0}"` )
+
+# Files with the following extension are considered templates and will
+# be expanded
+_TEMPLATE_SUFFIX=${TEMPLATE_SUFFIX:-".tmpl"}
+
+# Display an INFO message
+# $1: Message to display
+function _msg_info() {
+    local _msg="${1}"
+
+    echo "[${_SCRIPT_NAME}] INFO: ${_msg}"
+}
+
+# Display an ERROR message
+# $1: Message to display
+# $2: Exit code
+function _msg_error() {
+    local _msg="${1}"
+    local _rc=${2}
+
+    echo "[${_SCRIPT_NAME}] ERROR: ${_msg}"
+
+    if [[ ${_rc} -ne 0 ]]; then
+        exit ${_rc}
+    fi
+}
+
+# Prints usage info and exits
+function _usage() {
+    echo "${_SCRIPT_NAME} [expand|delete-templates]"
+    exit 64  # EX_USAGE
+}
+
+# Expands any template files
+function _expand_templates() {
+    for _tmpl_file in $( find . -name "*${_TEMPLATE_SUFFIX}" ); do
+        _expanded=$( echo "${_tmpl_file}" | sed -e "s|${_TEMPLATE_SUFFIX}||g" )
+        _msg_info "Expanding template file ${_tmpl_file} ..."
+        envsubst < "${_tmpl_file}" > "${_expanded}"
+    done
+}
+
+# Deletes any template files
+function _delete_templates() {
+    for _tmpl_file in $( find . -name "*${_TEMPLATE_SUFFIX}" ); do
+        _msg_info "Deleting template file ${_tmpl_file} ..."
+        rm -f "${_tmpl_file}"
+    done
+}
+
+# Main entrypoint
+function _main() {
+    local _cmd="${1}"
+
+    if [[ $# -lt 1 ]]; then
+        _usage
+    fi
+
+    local _project_vars="${_SCRIPT_DIR}/project-vars.env"
+
+    # Source project variables, if any.
+    if [ ! -f "${_project_vars}" ]; then
+	_msg_error "Skeleton variables not found: ${_project_vars}" 1
+
+    fi
+
+    set -a
+    source "${_project_vars}"
+    set +a
+
+    case "${_cmd}" in
+        expand)
+            _expand_templates
+            ;;
+        delete-templates)
+            _delete_templates
+            ;;
+        *)
+            _usage
+            ;;
+    esac
+
+    _msg_info "Done"
+}
+
+_main $*
